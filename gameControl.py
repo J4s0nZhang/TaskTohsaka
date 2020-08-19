44676faf283cbb938bacbc0b46655e15ced4acbb
JasonZhang <thejasonzhang@gmail.com>
2020-08-19 15:24:35 -0400

    tested basic functionality of pyautogui for controls

---
+++ gameControl.py
@@ -0,0 +1,47 @@
+"""
+Controls the game by taking screen shots of the game (currently takes screen shot of the whole screen)
+mouse and screen shots taken with pyautogui 
+"""
+
+import PIL 
+import numpy as np 
+import pyautogui
+import matplotlib.pyplot as plt 
+import sys 
+
+# click at input location 
+def click_at(x, y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+# prints the current mouse position 
+def print_mouse_pos():
+    print("Press Ctrl-C to quit")
+    try: 
+        while(True):
+            x, y = pyautogui.position()
+            print('X: %d, Y: %d' %(x,y))
+
+    except KeyboardInterrupt:
+        print("done")
+
+class ScreenGrabber:
+    def __init__(self, region=None):
+        self.image = []
+        self.region = (0,0,0,0)
+        if region:
+            self.region = size
+
+    def grab_screen(self):
+        if self.region != (0,0,0,0):
+            self.image = pyautogui.screenshot(region=region)
+        else:
+            self.image = pyautogui.screenshot()
+
+    def show_screen(self):
+        plt.imshow(self.image)
+        plt.show()
+
+if __name__ == "__main__":
+    print_mouse_pos()
+
